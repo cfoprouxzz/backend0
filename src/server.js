6d0fc8cc22ae19988390a981facf50e0ba01730c
cfoprouxzz <hoanganhcq@gmail.com>
2026-07-06 21:58:01 +0700

    20

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require('express')  // commonjs
+const path = require('path')
+require('dotenv').config()
+// import express from 'express'    // es modules
+
+const app = express()   // app express
+const port = process.env.PORT || 8888
+const hostname = process.env.HOST_NAME
+
+// config template engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// khai bao route
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/abc', (req, res) => {
+    res.send('check abc')
+})
+
+app.get('/aihoi', (req, res) => {
+    res.render('sample.ejs')
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Example app listening on port ${port}`)
+})
