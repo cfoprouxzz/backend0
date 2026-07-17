df8de600edcf9049c66319c4b86628f401dc9ec4
cfoprouxzz <hoanganhcq@gmail.com>
2026-07-18 02:20:12 +0700

    30-connect database(mysql2) with Docker

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 const express = require('express')  // commonjs
 const path = require('path')
 const configViewEngine = require('./config/viewEngine')
-const webRoutes = require('./routes/web')
+const webRoutes = require('./routes/web');
+
+const mysql = require('mysql2');
 
 // import express from 'express'    // es modules
 
@@ -16,6 +18,27 @@ configViewEngine(app);
 
 // khai bao route
 app.use('/v1', webRoutes)
+
+
+// test Connection
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,     // default 3306
+    user: 'root',
+    password: '123456',
+    database: 'hoanganh'
+});
+
+
+// simple query
+connection.query(
+    'SELECT * FROM `Users`',
+    function (err, results, fields) {
+        console.log(">>> results:", results);
+        console.log(">>>fields:", fields);
+    }
+);
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
