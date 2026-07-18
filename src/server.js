dd03c9ecc76f564da5b259ca297f5085c40256f7
cfoprouxzz <hoanganhcq@gmail.com>
2026-07-18 17:05:42 +0700

    31 - reuse Connection

--- src/server.js
+++ src/server.js
@@ -3,8 +3,9 @@ const express = require('express')  // commonjs
 const path = require('path')
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web');
+const connection = require('./config/database');
 
-const mysql = require('mysql2');
+
 
 // import express from 'express'    // es modules
 
@@ -21,14 +22,7 @@ app.use('/v1', webRoutes)
 
 
 // test Connection
-// create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307,     // default 3306
-    user: 'root',
-    password: '123456',
-    database: 'hoanganh'
-});
+
 
 
 // simple query
@@ -36,7 +30,7 @@ connection.query(
     'SELECT * FROM `Users`',
     function (err, results, fields) {
         console.log(">>> results:", results);
-        console.log(">>>fields:", fields);
+        // console.log(">>>fields:", fields);
     }
 );
 
