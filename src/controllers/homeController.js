2a033e36acd6ac603bb9f1833bcc53e241f913a1
cfoprouxzz <hoanganhcq@gmail.com>
2026-07-20 17:23:44 +0700

    38 - add new user to database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -35,8 +35,26 @@ const getAihoi = (req, res) => {
 
 
 const postCreateUser = (req, res) => {
-    console.log(">>> req.body:", req.body);
+
+    let email = req.body.email;
+    let name = req.body.name;
+    let city = req.body.city;
+
+    console.log(">>> email =", email, ", name =", name, ", city =", city);
+
+    // let { email, name, city } = req.body;
+
+
     res.send('create a new user');
+
+    connection.query(
+        `INSERT INTO Users(email, name, city) 
+        VALUES(?, ?, ?)`,
+        [email, name, city],
+        function (err, results) {
+            console.log(results);
+        }
+    );
 }
 
 module.exports = {
