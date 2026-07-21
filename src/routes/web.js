6d30049caf2a87692b5ef4803d151e121e554e7e
cfoprouxzz <hoanganhcq@gmail.com>
2026-07-21 16:25:20 +0700

    43 - Route Params

--- src/routes/web.js
+++ src/routes/web.js
@@ -11,7 +11,7 @@ router.get('/abc', getAbc);
 router.get('/aihoi', getAihoi);
 
 router.get('/create', getCreatePage);
-router.get('/update', getUpdatePage);
+router.get('/update/:id', getUpdatePage);
 
 router.post('/create-user', postCreateUser);
 
